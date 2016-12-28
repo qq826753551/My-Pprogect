8a99b6867c0b75ad0d012cc087f4045389c02b52
qq826753551 <826753551@qq.com>
2016-12-28 20:45:24 +0800

    first commit

---
+++ app/src/main/java/shop/beiw/com/rikao1228/MainActivity.java
@@ -0,0 +1,14 @@
+package shop.beiw.com.rikao1228;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //我是日考
+    }
+}
